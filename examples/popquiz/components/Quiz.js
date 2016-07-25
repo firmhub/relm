baeec8a8836ec8cefcf4e14e599e15fed36f6964
Amandeep Othi <amandeep@firmhub.ca>
2016-07-24 23:14:14 -0400

    Working stats

--- examples/popquiz/components/Quiz.js
+++ examples/popquiz/components/Quiz.js
@@ -2,21 +2,49 @@ import _ from 'lodash';
 import * as UI from './ui';
 import Flashcard from './Flashcard';
 
+// Takes a set of topics and finds a random question
+// to display (omitting any skipped topics)
+//
+// topics: dict(ID, Topic) -> skippedTopics: dict(ID, Bool) -> state: Obj
 function shuffleTopics (topics, skippedTopics = {}) {
-  const randomTopic = _.sample(_.omitBy(topics, (it, id) => skippedTopics[id]));
-  const question = _.sample(randomTopic.questions);
-  const options = _.shuffle(question.o);
-  return { topics, question, options };
+  // Get a random topic and question
+  const topic = _.chain(topics).keys().reject(id => skippedTopics[id]).sample().value();  // Str
+  const questions = _.get(topics, [topic, 'questions']);      // dict(Num, Obj)
+  const shuffled = _.shuffle(questions);
+  const weakest = _.find(shuffled, q => (q.confidence || 0) < 150) || _.first(shuffled);
+
+  // Shuffle the question's options
+  const question = _.findKey(questions, x => x === weakest);  // Str
+  const options = _.shuffle(questions[question].o);
+
+  // Set the resulting properties on state
+  return { topics, topic, question, options, selection: null };
+}
+
+function getQuestion (state) {
+  return _.get(state, ['topics', state.topic, 'questions', state.question]);
+}
+
+function incrementStats (obj, attempt) {
+  if (attempt.correct) obj.correct = (obj.correct || 0) + 1;
+  obj.total = (obj.total || 0) + 1;
+  return obj;
 }
 
 export default function Quiz (h, { actions, state, components: x }) {
-  const question = state.question;
+  const question = getQuestion(state);
   if (!question) return null;
+
+  const topicStats = _.reduce(state.attempts, (agg, attempt) => {
+    agg[attempt.topic] = incrementStats(agg[attempt.topic] || {}, attempt);
+    return agg;
+  });
 
   return (
     <div style={{ padding: '2rem' }}>
       <x.Navigation
         topics={state.topics}
+        stats={topicStats}
         skippedTopics={state.skippedTopics}
         onToggle={actions.toggleTopic}
       />
@@ -27,7 +55,7 @@ export default function Quiz (h, { actions, state, components: x }) {
         answer={_.first(question.o)}
         options={state.options}
         selection={state.selection}
-        onChange={actions.checkAnswer}
+        onChange={actions.updateSelection}
         onNext={actions.nextQuestion}
       />
     </div>
@@ -42,7 +70,7 @@ Quiz.components = {
 
 Quiz.actions = {
   initializeState (state) {
-    return state.merge({ question: null, skippedTopics: {} });
+    return state.merge({ question: null, skippedTopics: {}, attempts: [] });
   },
 
   toggleTopic (state, topicId) {
@@ -59,16 +87,24 @@ Quiz.actions = {
 
   updateTopics (state, topics) {
     const shuffled = shuffleTopics(topics, state.skippedTopics);
-    return state.merge(shuffled).set('selection', null);
+    return state.merge(shuffled);
   },
 
-  checkAnswer (state, value) {
-    return state.set('selection', value);
+  updateSelection (state, value) {
+    // A selection was made, so update the question's confidence based on answer
+    const question = getQuestion(state);
+    const answerIsCorrect = value === _.first(question.o);
+    const change = (question.confidence || 0) + (answerIsCorrect ? 100 : -50);
+    const confidence = _.max([-100, _.min([change, 200])]);
+    const path = ['topics', state.topic, 'questions', state.question, 'confidence'];
+
+    // Save the selection and the confidence
+    return state.set('selection', value).set(path, confidence);
   },
 
   nextQuestion (state) {
     const shuffled = shuffleTopics(state.topics, state.skippedTopics);
-    return state.merge(shuffled).set('selection', null);
+    return state.merge(shuffled);
   }
 };
 
@@ -85,9 +121,17 @@ function Navigation (h, { props, styles }) {
       [styles.active]: !props.skippedTopics[id]
     };
 
+    // console.log(_.map(topic.questions, q => q.confidence || 0));
+
+    const confidence = _.chain(topic.questions)
+      .map(q => q.confidence || 0)
+      .mean()
+      .floor()
+      .value();
+
     return (
       <a className={classes} onClick={() => props.onToggle(id)}>
-        <span className={styles.count}>{_.size(topic.questions)}</span>
+        <span className={styles.count}>{confidence}</span>
         <span className={styles.label}>{topic.label}</span>
       </a>
     );
